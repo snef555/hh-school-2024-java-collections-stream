32095a98b3663438052ba2c48e0bd4daaf2a8e84
Sergei Nefedov <snef555@gmail.com>
2024-12-14 19:38:41 +0600

    Solving tasks of sorting collections

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -3,12 +3,13 @@ package tasks;
 import common.Person;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
 /*
 Задача 2
 На вход принимаются две коллекции объектов Person и величина limit
-Необходимо объеденить обе коллекции
+Необходимо объединить обе коллекции
 отсортировать персоны по дате создания и выдать первые limit штук.
  */
 public class Task2 {
@@ -16,6 +17,11 @@ public class Task2 {
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                      Collection<Person> persons2,
                                                      int limit) {
-    return new ArrayList<>();
+    Collection<Person>  resultCollection = new ArrayList<>();
+
+    resultCollection.addAll(persons1);
+    resultCollection.addAll(persons2);
+
+    return resultCollection.stream().sorted(Comparator.comparing(Person::createdAt)).limit(limit).toList();
   }
 }
